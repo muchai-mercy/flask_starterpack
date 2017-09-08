0cafd12eafe8d2fe8f12cd4ff7318c5562c2e6d6
Mercy-Muchai <mercy.muchai@andela.com>
2017-09-08 10:40:01 +0300

    macro form to render wtforms

--- forms.py
+++ forms.py
@@ -4,8 +4,8 @@ from wtforms.fields.html5 import URLField
 from wtforms.validators import DataRequired, url
 
 class BookmarkForm(Form):
-  url = URLField('url', validators=[DataRequired(), url()])
-  description = StringField('description')
+  url = URLField('The URL for your bookmark:', validators=[DataRequired(), url()])
+  description = StringField('Add an optional description:')
   
   def validate(self):
     if not self.url.data.startswith("http://") or\
